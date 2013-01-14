28e4816c938ed864585d9b8ded32a78274c0a5a6
crijop <crijop@gmail.com>
2013-01-14 17:08:07 +0000

    assertEquals

--- src/PyUnitABCP.py
+++ src/PyUnitABCP.py
@@ -3,3 +3,18 @@ Created on 2013/01/14
 
 @author: admin1
 '''
+
+
+
+
+
+def assertEqualsPy(expected, actual):
+    if not type(expected) == type(actual):
+        print "false"
+        return False
+    else:
+        print "vrdd"
+        return True
+
+assertEqualsPy(1, "oala") 
+    
