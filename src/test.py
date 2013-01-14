3679ba322316c62c1ecf270a5dcd9f11fa76edbc
91195604 <91195604@alunos.ipbeja.pt>
2013-01-14 18:38:24 +0000

    Ficheiro test.py

---
+++ src/test.py
@@ -0,0 +1,22 @@
+#-*- coding:utf-8 -*-
+'''
+Created on 14 de Jan de 2013
+
+@author: xama
+'''
+
+from PyUnitABCP import *
+
+dir()
+
+@Test
+def primeiroTeste():
+    
+    i = 5
+    
+    print "teste", assertEqualsPy("vgh", i)
+    pass
+    
+    
+#primeiroTeste()
+    
