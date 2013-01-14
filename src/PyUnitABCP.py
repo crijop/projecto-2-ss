8bbe57e5ff3805da8640f0a736ab6ac21764928f
crijop <crijop@gmail.com>
2013-01-14 20:13:03 +0000

    Novos asserts

--- src/PyUnitABCP.py
+++ src/PyUnitABCP.py
@@ -26,8 +26,6 @@ class Test(object):
         pass
     
 
-
-
 def assertEqualsPy(expected, actual):
     mensage = ""
     if isTest == True:
@@ -47,7 +45,56 @@ def assertEqualsPy(expected, actual):
     else:
         mensage += "Não se verifica um teste (@Test)"
         print mensage
-        print "não é teeste"
+        pass
+    pass
                 
-            
+def assertTruePy(condition):
+    mensage = ""
+    if isTest == True:
+        if type(condition) == bool:
+            if condition == True:
+                mensage += "A variavél é verdadeira"
+                print mensage
+                return True
+            else:
+                mensage += "A variavél é falsa"
+                print mensage
+                return False
+            pass
+        else:
+            mensage += "A variavél não é do tipo Booleana"
+            print mensage
+            return False
+            pass
+        pass
+    else:
+        mensage += "Não se verifica um teste (@Test)"
+        print mensage
+        pass
+    pass
 
+def assertFalsePy(condition):
+    mensage = ""
+    if isTest == True:
+        if type(condition) == bool:
+            if condition == False:
+                mensage += "A variavél é Falsa"
+                print mensage
+                return True
+            else:
+                mensage += "A variavél é Verdadeira"
+                print mensage
+                return False
+            pass
+        else:
+            mensage += "A variavél não é do tipo Booleana"
+            print mensage
+            return False
+            pass
+        pass
+    else:
+        mensage += "Não se verifica um teste (@Test)"
+        print mensage
+        pass
+            
+    
--- src/test.py
+++ src/test.py
@@ -12,10 +12,31 @@ dir()
 @Test
 def primeiroTeste():
     
-    i = 5
-    assertEqualsPy(5, i)
+    array = [1, 2, 4]
+    i = "carlos"
+    a = 1
+    assertEqualsPy("carlos", i)
+    assertEqualsPy(1, a)
+    assertEqualsPy(array, [1, 2, 4, 4])
     pass
-    
-    
-primeiroTeste()
+
+@Test
+def segundoTeste():
+    var = True
+    assertTruePy(var)
+    pass
+
+@Test
+def terceiroTeste():
+    var = False
+    assertFalsePy(var)
+    pass
+
+if __name__ == "__main__":
+    primeiroTeste()
+    segundoTeste()
+    terceiroTeste()
+    pass
+
+
     
